532d460175a53402929ff4bbb348ba55f9d4b376
Ivelin Bachvarov <IPBachvarov18@codingburgas.bg>
2021-03-06 12:31:49 +0200

    Add edit menu's for teacher, student and school
    Begin work on #6, #7, #8

--- error-by-night/error-by-night/console_io.cpp
+++ error-by-night/error-by-night/console_io.cpp
@@ -93,6 +93,61 @@ void menuStore(const vector<SCHOOL> &schools)
 	storeSchools(schools, "backup");
 }
 
+bool menuSchool(SCHOOL &school)
+{
+	size_t choice;
+	vector<const char *> options = {"Edit name", "Edit city", "Edit address",
+									"Select student", "List students",
+									"Select teacher", "List teacher",
+									"Select team", "List team",
+									"Select project", "List project",
+									"Quit"};
+
+	displayMenuOptions(options);
+	getMenuChoice(choice, options.size());
+
+	switch (choice) {
+		case 1:
+			menuEditName(school);
+			break;
+		case 2:
+			menuEditCity(school);
+			break;
+		case 3:
+			menuEditAddress(school);
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+
+			break;
+		case 7:
+
+			break;
+		case 8:
+
+			break;
+		case 9:
+
+			break;
+		case 10:
+
+			break;
+		case 11:
+
+			break;
+
+		case 12:
+			return false;
+	}
+
+	return true;
+}
+
 void menuEditName(SCHOOL &school)
 {
 	cout << "Current name: " << school.name << endl;
@@ -112,6 +167,27 @@ void menuEditAddress(SCHOOL &school)
 	cout << "Current address: " << school.address << endl;
 	cout << "Enter new school address: ";
 	getline(cin, school.address);
+}
+
+void menuEditFirstName(STUDENT &student)
+{
+	cout << "Current first name: " << student.firstName << endl;
+	cout << "Enter new first name: ";
+	getline(cin, student.firstName);
+}
+
+void menuEditLastName(STUDENT &student)
+{
+	cout << "Current last name: " << student.lastName << endl;
+	cout << "Enter new last name: ";
+	getline(cin, student.lastName);
+}
+
+void menuEditEmail(STUDENT &student)
+{
+	cout << "Current email: " << student.email << endl;
+	cout << "Enter new email: ";
+	getline(cin, student.email);
 }
 
 void menuEditFirstName(TEACHER &teacher)
--- error-by-night/error-by-night/console_io.h
+++ error-by-night/error-by-night/console_io.h
@@ -7,9 +7,21 @@ void menuDriverMain(std::vector<SCHOOL> &schools);
 bool menuMain(std::vector<SCHOOL> &schools);
 void menuRestore(std::vector<SCHOOL> &schools);
 void menuStore(const std::vector<SCHOOL> &schools);
-void menuEditName(SCHOOL &schools);
-void menuEditCity(SCHOOL &schools);
-void menuEditAddress(SCHOOL &schools);
+
+bool menuSchool(SCHOOL &school);
+
+void menuEditName(SCHOOL &school);
+void menuEditCity(SCHOOL &school);
+void menuEditAddress(SCHOOL &school);
+
+bool menuTeacher(TEACHER &teacher);
+
 void menuEditFirstName(TEACHER &teacher);
 void menuEditLastName(TEACHER &teacher);
 void menuEditEmail(TEACHER &teacher);
+
+bool menuStudent(STUDENT &student);
+
+void menuEditFirstName(STUDENT &student);
+void menuEditLastName(STUDENT &student);
+void menuEditEmail(STUDENT &student);
